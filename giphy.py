34885d4e43f9753ffc3b1a5f355afb7252cac6d9
alexsh2ko <124434801+alexsh2ko@users.noreply.github.com>
2023-03-17 00:49:42 +0200

    Add function

--- giphy.py
+++ giphy.py
@@ -1,12 +1,29 @@
 import requests
-import json
 import time
 
-key = "ifxcN2DInREnFe83Ym1OABrD2zP2bsBj"
 user_word = input("Введіть ваше слово: ")
-url = f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={user_word}"
-resp_search = requests.get(url)
-resp_json = resp_search.json()
-for gif in resp_json["data"]:
-    print(gif["url"])
+
+
+def give_giphy(user_word: str) -> str:
+    '''
+    The function accepts a request from a user and
+    displays the search result based on the user's input from giphy.com
+
+    Parameters
+    ----------
+        user_word : the variable contains a string from user input
+
+    Returns
+    -------
+    Displaying user search results from giphy.com
+    '''
+    key = "ifxcN2DInREnFe83Ym1OABrD2zP2bsBj"
+    url = f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={user_word}"
+    resp_search = requests.get(url)
+    resp_json = resp_search.json()
+    for gif in resp_json["data"]:
+        return print(gif["url"])
+
+
+give_giphy(user_word)
 time.sleep(15)
--- weather_search.py
+++ weather_search.py
@@ -1,30 +1,47 @@
 import requests
 import time
-api_key = "6f6a636241627bb40d71f0a52a2cf100"
 us_city = input("Enter city name: ")
+api_key = "6f6a636241627bb40d71f0a52a2cf100"
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={us_city}&appid={api_key}&units=metric"
-response = requests.get(url)
-resp_weather = response.json()
-if response.status_code == 200:
-    summary_res = response.json()
-    temp = summary_res["main"]["temp"]
-    feels_like = summary_res["main"]["feels_like"]
-    description = summary_res["weather"][0]["description"]
-    temp_min = summary_res["main"]["temp_min"]
-    temp_max = summary_res["main"]["temp_max"]
-    humidity = summary_res["main"]["humidity"]
-    sea_level = summary_res["main"]["sea_level"]
-    grnd_level = summary_res["main"]["grnd_level"]
-    wind = summary_res["wind"]["speed"]
-    print(f"Current weather in {us_city}: \n{description}"
-          f"\ntemperature is {temp}°C but feels like {feels_like}°C"
-          f"\nmin temperature is {temp_min}"
-          f"\nmax temperature is {temp_max}"
-          f"\nhumidity is {humidity}"
-          f"\nsea level is {sea_level}"
-          f"\ngrnd level is {grnd_level}"
-          f"\nwind speed is {wind}")
-else:
-    print("Error retrieving weather information.")
+
+def weather_searching(us_city: str, api_key: str) -> str:
+    '''
+    The function accepts a request from a user and
+    displays information about the weather
+    based on the user's city input from openweathermap.org
+
+    Parameters
+    ----------
+        us_city : the variable contains a string from user input
+        api_key : the variable contains a string
+                  with api key from openweathermap.org
+
+    Returns
+    -------
+    Displaying information about the weather in user city
+    '''
+    response = requests.get(f"https://api.openweathermap.org/"
+                            f"data/2.5/weather?q={us_city}"
+                            f"&appid={api_key}&units=metric")
+    resp_weather = response.json()
+    if resp_weather.status_code == 200:
+        summary_res = response.json()
+        temp = summary_res["main"]["temp"]
+        feels_like = summary_res["main"]["feels_like"]
+        description = summary_res["weather"][0]["description"]
+        temp_min = summary_res["main"]["temp_min"]
+        temp_max = summary_res["main"]["temp_max"]
+        humidity = summary_res["main"]["humidity"]
+        wind = summary_res["wind"]["speed"]
+        return print(f"\nCurrent weather in {us_city}:\n \n{description}"
+                     f"\ntemperature is {temp}°C but feels like {feels_like}°C"
+                     f"\nmin temperature is {temp_min}"
+                     f"\nmax temperature is {temp_max}"
+                     f"\nhumidity is {humidity}"
+                     f"\nwind speed is {wind}")
+    else:
+        print("Error retrieving weather information.")
+
+
+weather_searching(us_city, api_key)
 time.sleep(7)
